d7642bf433203db8455c240d40b297a2cb6d1dca
theRunCom <1772414249@qq.com>
2022-09-01 16:31:08 +0800

    Create hello_world

---
+++ hello_world/target/debug/deps/hello_world-1a0d0a4e48211ce8.d
@@ -0,0 +1,5 @@
+/home/zzl/src/Rustdemo/hello_world/target/debug/deps/hello_world-1a0d0a4e48211ce8.rmeta: src/main.rs
+
+/home/zzl/src/Rustdemo/hello_world/target/debug/deps/hello_world-1a0d0a4e48211ce8.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-7b578fb5c569634f.d
@@ -0,0 +1,5 @@
+/home/zzl/src/Rustdemo/hello_world/target/debug/deps/hello_world-7b578fb5c569634f.rmeta: src/main.rs
+
+/home/zzl/src/Rustdemo/hello_world/target/debug/deps/hello_world-7b578fb5c569634f.d: src/main.rs
+
+src/main.rs:
